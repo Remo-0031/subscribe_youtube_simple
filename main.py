c88616eb4628151f4471db874ccc4e14bf013dc9
Remo Del Rosario <2227393@slu.edu.ph>
2025-05-23 22:25:02 +0800

    rename of file

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from time import sleep
 
 import pyautogui
 
-
-pyautogui.hotkey("ctrl","t")
+pyautogui.hotkey("alt", "tab")
+pyautogui.hotkey("ctrl", "t")
 
 pyautogui.write("youtube.com")
 pyautogui.hotkey("enter")
 
-sleep(3)
+sleep(4)
 search_button = pyautogui.locateCenterOnScreen("search_youtube.jpg", confidence=0.8)
 pyautogui.moveTo(search_button)
 pyautogui.click()
@@ -23,6 +23,9 @@ pyautogui.click()
 
 sleep(1)
 
-subscribe = pyautogui.locateCenterOnScreen("subscribe.png",confidence=0.9)
-pyautogui.moveTo(subscribe)
-pyautogui.click()
+try:
+    subscribe = pyautogui.locateCenterOnScreen("subscribe.png", confidence=0.9)
+    pyautogui.moveTo(subscribe)
+    pyautogui.click()
+except pyautogui.ImageNotFoundException:
+    print("you are already subscribed")
