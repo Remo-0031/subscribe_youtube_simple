50eb5e72fe35393f0c9f9b9386a59aaf61a43c14
Remo Del Rosario <2227393@slu.edu.ph>
2025-05-23 22:19:14 +0800

    The main file

---
+++ sweeper.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+import pyautogui
+
+
+pyautogui.hotkey("ctrl","t")
+
+pyautogui.write("youtube.com")
+pyautogui.hotkey("enter")
+
+sleep(3)
+search_button = pyautogui.locateCenterOnScreen("search_youtube.jpg", confidence=0.8)
+pyautogui.moveTo(search_button)
+pyautogui.click()
+
+pyautogui.write("Coding 101 with Steve")
+pyautogui.hotkey("enter")
+
+sleep(2)
+coding_steve = pyautogui.locateCenterOnScreen("coding_steve.jpg", confidence=0.8)
+pyautogui.moveTo(coding_steve)
+pyautogui.click()
+
+sleep(1)
+
+subscribe = pyautogui.locateCenterOnScreen("subscribe.png",confidence=0.9)
+pyautogui.moveTo(subscribe)
+pyautogui.click()
